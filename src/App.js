6804e406ce9716e44c3dc4e295079d2b8937895a
teepiik <teemu.piik@gmail.com>
2018-04-17 18:55:30 +0300

    latest

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import BeerForm from './components/BeerForm'
 import Menu from './components/Menu'
 import Notification from './components/Notification'
 import Frontpage from './components/Frontpage'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route} from 'react-router-dom'
 import BeerListing from './components/BeerListing';
 
 class App extends React.Component {
@@ -45,9 +45,26 @@ class App extends React.Component {
     }, 5000)
   }
 
-  beerByid = (id) => this.state.beers.find(b => b.id === id)
+  handleDelete = async (event, beer) => {
+    const result = window.confirm('Are you sure to delete this?')
+    if(result) {
+      console.log(beer)
+      await beerService.destroy(beer.id)
+      this.setState({
+        beers: this.state.beers.filter(b => b.id.toString() !== beer.id),
+        message: `Successful deletion`
+      })
+      setTimeout(() => {
+        this.setState({ message: '' })
+      }, 5000)
+    }
+  }
+
+  
 
   render() {
+    const beerByid = (id) => this.state.beers.find(b => b.id === id)
+
     return (
       <div>
         <Router>
@@ -55,9 +72,9 @@ class App extends React.Component {
             <Menu />
             <Notification message={this.state.message} />
             <Route exact path="/" render={() => <Frontpage />} />
-            <Route exact path="/createbeer" render={({ history }) => <BeerForm history={history} addBeer={this.addBeer} />} />
+            <Route path="/createbeer" render={({ history }) => <BeerForm history={history} addBeer={this.addBeer} />} />
             <Route exact path="/beers" render={() => <BeerListing beers={this.state.beers} />} />
-            <Route exact path="/beers/:id" render={({match}) => <Beer beer={this.beerByid(match.params.id)} />} />
+            <Route exact path="/beers/:id" render={({match}) => <Beer beer={beerByid(match.params.id)} handleDelete={this.handleDelete} />} />
           </div>
         </Router>
       </div>
--- src/components/Beer.js
+++ src/components/Beer.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import { Button } from 'react-bootstrap'
 
-const Beer = ({ beer }) => (
-        <div className="beer">
-            <h3>{beer.name}</h3>
-            <p>type: {beer.type}, alcohol%: {beer.alcohol_percent}</p>
-            <p>brewery: {beer.brewery}, country of origin: {beer.country}</p>
-        </div>
-    )
+const Beer = ({ beer, handleDelete }) => (
+    <div className="beer">
+        <h3>{beer.name}</h3>
+        <p>type: {beer.type}, alcohol%: {beer.alcohol_percent}</p>
+        <p>brewery: {beer.brewery}, country of origin: {beer.country}</p>
+
+        <form onSubmit={handleDelete}>
+            <Button bsStyle="success" type="submit">delete</Button>
+        </form>
+    </div>
+)
 
 export default Beer
